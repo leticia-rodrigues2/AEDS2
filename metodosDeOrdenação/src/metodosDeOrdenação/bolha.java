563f7ba06cced34637cf151f5c9c80cd29125bf4
leticia-rodrigues2 <leticiaarodrigues2@gmail.com>
2021-04-28 18:57:57 -0300

    modelo método de ordenação bolha exemplo

--- metodosDeOrdenação/src/metodosDeOrdenação/bolha.java
+++ metodosDeOrdenação/src/metodosDeOrdenação/bolha.java
@@ -1,10 +1,30 @@
 package metodosDeOrdenação;
 
+import java.util.Arrays;
+
 public class bolha {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int [] v= {55,35,45,70,22,10,2,14,28,89};
+		bolha(v);
+		System.out.println(Arrays.toString(v));
+	}
 
+	private static void bolha(int[] v) {
+		for (int ultimo =v.length-1; ultimo >0; ultimo--) {
+			for (int i=0; i< ultimo; i++) 
+				if(v[i] > v[i+1]) 
+				trocar(v,i,i+1);
+				
+		}
+	}
+
+	private static void trocar(int[] v, int i, int j) {
+		int aux = v[i];
+		v[i]=v[j];
+		v[j]=aux;
+		
 	}
 
 }
